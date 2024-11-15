42f8315011631d89f0824595f4261cfa40c697bb
ep1088 <ncwsky@qq.com>
2024-11-15 16:10:27 +0800

    精简模板

--- tpl/Base.php
+++ tpl/Base.php
@@ -3,7 +3,6 @@ namespace __app__\control;
 
 //基类
 class Base extends \myphp\Control{
-	// 构造函数
     public function __construct() {
 		parent::__construct();
     }
--- tpl/IndexAct.php
+++ tpl/IndexAct.php
@@ -4,16 +4,10 @@ namespace __app__\control;
 class __c__ extends Base{
 	//创建一个方法
 	public function __a__(){
-/*
-		$res = db()->query("select * from test");	//查询数据库
-		$row = db()->fetch($res);	//获得查询结果
-		return $row;	//输出结果
-*/
-		$title = '欢迎信息';
 		//赋值给模板变量
-        $this->assign('title', $title);
+        $this->assign('title', 'Hi');
         return $this->fetch('index.html', [
-            'mess' => '我是一个MVC框架'
+            'mess' => '__app__'
         ]);
 /*
         //还可以与assign混合使用 此处在__construct 时特别有用 可以对一些需要全局使用的模板变量进行赋值调用
