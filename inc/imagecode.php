6d19eafe8772c92b448a7e1627456793864d72af
ep1088 <ncwsky@qq.com>
2024-12-08 11:04:46 +0800

    cs

--- inc/imagecode.php
+++ inc/imagecode.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 require_once __DIR__ . '/../ext/Image.php';
 
 $w = (int)($_GET['w'] ?? 80);
@@ -8,7 +10,7 @@ $size = (int)($_GET['size'] ?? 18);
 $len = (int)($_GET['len'] ?? 4);
 $type = (int)($_GET['number'] ?? 0);
 
-Image::code($w, $h, $size, $len, $type, function ($code) {
+Image::code($w, $h, $size, $len, $type, function ($code): void {
     if (!isset($_SESSION)) { //如果没有开启，session，则开启session
         session_start();
     }
